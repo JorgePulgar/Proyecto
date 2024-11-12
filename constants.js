696ceaa9f038ecc808d402575fca6789b97553c2
Jorge <jpulgar111@gmail.com>
2024-11-12 08:44:58 +0100

    .

--- constants.js
+++ constants.js
@@ -1,1 +1,6 @@
 export const COMPANY = 'IES Pío Baroja'
+
+//forma default
+export default {
+    COMPANY: 'IES Pío Baroja',
+}
--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//FORMA SIMPLE
 import { COMPANY } from "./constants.js";
 
-console.log('hello world' + COMPANY)
+console.log('hello world ' + COMPANY)
+
+//FORMA DEFAULT
+import constants from "./constants.js";
+
+console.log('Compañía ' + constants.COMPANY)
